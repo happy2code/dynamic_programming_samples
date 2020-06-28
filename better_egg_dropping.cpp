2ea037ead35f02640557ef0a2c2605c6b1814fce
Senthilvel Samatharman <s.senthilvel@gmail.com>
2020-06-28 20:07:09 +0530

    finishing touces for egg dropping problem

--- better_egg_dropping.cpp
+++ better_egg_dropping.cpp
@@ -1,14 +1,20 @@
 #include <iostream>
 #include <limits>
 #include <algorithm>
+#include <map>
 using namespace std;
 
+map<unsigned int, map<unsigned int,unsigned int>> cache;
 
 unsigned int GetOptimumAttempts(unsigned int no_of_floors, unsigned int number_of_eggs)
 {
     if(number_of_eggs == 1) return no_of_floors;
     if(no_of_floors == 1) return no_of_floors;
-   
+    if(cache[no_of_floors][number_of_eggs] != 0)
+    {
+        return cache[no_of_floors][number_of_eggs];
+    }
+
     auto min_num_attempts = numeric_limits<unsigned int>::max();
     for(auto currentfloor = 1; currentfloor < no_of_floors; currentfloor++) {
         //if egg breaks, you have one egg less and you need to search in the floors less than the floor number
@@ -24,6 +30,7 @@ unsigned int GetOptimumAttempts(unsigned int no_of_floors, unsigned int number_o
         }
             
     }
+    cache[no_of_floors][number_of_eggs] = min_num_attempts; 
     return min_num_attempts;
 }
 
